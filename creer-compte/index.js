88feb15d0ecee2c6e4d621de27a30ec3595b4d65
Samuel <samuel210@live.fr>
2021-06-19 06:50:19 +0200

    Correction page de connexion verification email,  et creation page de creation de compte. Tout fonctionne correctement

--- creer-compte/index.js
+++ creer-compte/index.js
@@ -1,25 +1,35 @@
 const submit = document.getElementById("valider");
+const nom = document.getElementById("nom");
+const prenom = document.getElementById("prenom");
 const email = document.getElementById("email");
 const motdepasse = document.getElementById("mp");
 const erreur = document.getElementById("erreur");
+const succes = document.getElementById("succes");
 const re =
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 submit.addEventListener("click", (e) => {
     e.preventDefault();
 
-    if (email.value == "" || motdepasse.value == "")
+    if (
+        nom.valuer == "" ||
+        prenom.value == "" ||
+        email.value == "" ||
+        motdepasse.value == ""
+    )
         erreur.textContent = "Veuillez renseigner les champs svp!";
     else if (re.test(email.value) == false)
         erreur.textContent = "Veuillez renseigner un email valide svp";
     else {
-        const API_URL = "https://api-taaches-smarone.herokuapp.com/login";
+        const API_URL = "https://api-taaches-smarone.herokuapp.com/signin";
 
         const REQUEST_HEADERS = {
             "Content-Type": "application/json",
         };
 
         const donneeUtilisateur = {
+            nom: nom.value,
+            prenom: prenom.value,
             email: email.value,
             mp: motdepasse.value,
         };
@@ -29,16 +39,11 @@ submit.addEventListener("click", (e) => {
         axios
             .post(API_URL, formattedData, { headers: REQUEST_HEADERS })
             .then((response) => {
-                if (response.data.error) erreur.textContent = "Identifiant incorrectes";
-                if (response.data.id) {
-                    localStorage.setItem(
-                        "x-access-token",
-                        response.headers.authorization
-                    );
-                    sessionStorage.setItem("utilisateur", response.data.id);
-                    window.location.replace(
-                        "https://taaches-smarone.netlify.app/accueil/"
-                    );
+                if (response.data.error)
+                    erreur.textContent = "Utilisateur existe déja!";
+                if (response.data.succes) {
+                    erreur.textContent = "";
+                    succes.textContent = "Utilisateur crée !";
                 }
             })
             .catch((error) => console.error(error));
